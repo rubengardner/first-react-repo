ca67078bdfc0f2cfae62a6bf3d546e12abf46cbd
rubengardner <rgardnercuesta@gmail.com>
2023-04-13 16:05:24 +0000

    More components added

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import UseEffectCounter from './components/UseEffectCounter';
+import UseEffectCounterContainer from './components/UseEffectCounterContainer';
 
 import UseStateWithArrays from './components/UseStateWithArrays';
 import UseStateWithObjects from './components/UseStateWithObjects';
@@ -8,7 +9,7 @@ import UseStateWithObjects from './components/UseStateWithObjects';
 function App() {
   return (
     <div className="App">
-       <UseEffectCounter />
+       <UseEffectCounterContainer />
     </div>
   );
 }
--- react-app/src/components/UseEffectCounter.js
+++ react-app/src/components/UseEffectCounter.js
@@ -2,18 +2,31 @@ import React, {useState, useEffect} from 'react'
 
 function UseEffectCounter() {
     const [count, setCount] = useState(0);
-    useEffect(()=> {
+    const [time, setTime] = useState(0);
+
+    useEffect(() => {
+        console.log('count 1 effect');
         document.title = count;
-    });
-  return (
-    <div>
-        <button onClick={
-            ()=> setCount(count => count + 1)
-        }>
-            Increment count: {count}
-        </button>
-    </div>
-  )
+    }, [count]);
+    useEffect(()=>{
+        console.log('Creating timer');
+        const interval = setInterval(() => {
+            setTime(time => time + 1)
+        }, 1000);
+        return () => {
+            return clearInterval(interval)
+        }
+    }, []);
+    return (
+        <div>
+            <button onClick={
+                () => setCount(count => count + 1)
+            }>
+                Increment Count ({count})
+            </button>
+            <h2>Time is {time}</h2>
+        </div>
+    )
 }
 
 export default UseEffectCounter
